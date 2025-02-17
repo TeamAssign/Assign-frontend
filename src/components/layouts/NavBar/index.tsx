8beda900a16d24b3c35e6e1335d3d0b1421b1326
min-s9709 <mim970929@naver.com>
2025-02-17 16:22:23 +0900

    feat: NavBar 링크 연결

--- src/components/layouts/NavBar/index.tsx
+++ src/components/layouts/NavBar/index.tsx
@@ -2,32 +2,45 @@ import HistoryIcon from '@/assets/icons/HistoryIcon.svg?react'
 import HomeIcon from '@/assets/icons/HomeIcon.svg?react'
 import TeamIcon from '@/assets/icons/TeamIcon.svg?react'
 import UserIcon from '@/assets/icons/UserIcon.svg?react'
+import { Link } from 'react-router-dom'
 
 /**
  * NavBar 컴포넌트
  * @description
- * NavBar 부분도 나중에 라우팅 연결하면 Link 를 통해 페이지 연결 예정입니다. 현재는 퍼블리싱만 해둔 상태입니다.
+ * TODO: 페이지 이동 시 색상 변경
  */
 
 const NavBar = () => {
   return (
     <div className='fixed bottom-0 w-full z-50 max-w-[600px] border-t-[1px] bg-white border-gray-200 p-3 h-16'>
       <ul className='flex w-full h-full items-center justify-between text-subbody text-[#666666]'>
-        <li className='flex flex-col items-center gap-1 p-1'>
-          <HomeIcon />
-          <span>홈</span>
+        <li>
+          <Link className='flex flex-col items-center gap-1 p-1' to='/'>
+            <HomeIcon />
+            <span>홈</span>
+          </Link>
         </li>
-        <li className='flex flex-col items-center gap-1 p-1'>
-          <TeamIcon />
-          <span>팀 피드</span>
+
+        <li>
+          <Link
+            className='flex flex-col items-center gap-1 p-1'
+            to='/teams/3141341'
+          >
+            <TeamIcon />
+            <span>팀 피드</span>
+          </Link>
         </li>
-        <li className='flex flex-col items-center gap-1 p-1'>
-          <HistoryIcon />
-          <span>히스토리</span>
+        <li>
+          <Link className='flex flex-col items-center gap-1 p-1' to='history'>
+            <HistoryIcon />
+            <span>히스토리</span>
+          </Link>
         </li>
-        <li className='flex flex-col items-center gap-1 p-1'>
-          <UserIcon />
-          <span>개인 피드</span>
+        <li>
+          <Link className='flex flex-col items-center gap-1 p-1' to='/my'>
+            <UserIcon />
+            <span>개인 피드</span>
+          </Link>
         </li>
       </ul>
     </div>
