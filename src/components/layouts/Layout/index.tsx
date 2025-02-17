1881f4187f096df9f3f1d827de90c48d2859be78
min-s9709 <mim970929@naver.com>
2025-02-17 16:21:53 +0900

    fix: Outlet 적용

--- src/components/layouts/Layout/index.tsx
+++ src/components/layouts/Layout/index.tsx
@@ -1,21 +1,20 @@
 import { Header, NavBar } from '@/components'
-
-interface LayoutProps {
-  children: React.ReactNode
-}
+import { Outlet } from 'react-router-dom'
 
 /**
  * Layout 컴포넌트
  * @description
- * 레이아웃을 잡기 위한 컴포넌트입니다 . 추후 라우팅 연결 후 Outlet으로 변경 예정입니다.
+ * 레이아웃을 잡기 위한 컴포넌트입니다 .
  */
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = () => {
   return (
     <div className='flex flex-col mx-auto min-h-screen max-w-[600px]'>
       <Header />
       <main className='w-full min-h-[calc(100vh-56px-64px)] py-14'>
-        <section className='p-3'>{children}</section>
+        <section className='p-3'>
+          <Outlet />
+        </section>
       </main>
       <NavBar />
     </div>
