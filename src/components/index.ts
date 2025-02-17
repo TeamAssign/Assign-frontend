c5b5520dd02820b1f1fd597f411377f466deb249
min-s9709 <mim970929@naver.com>
2025-02-17 16:21:14 +0900

    chore: 파일 내보내기

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from '@/components/layouts/Header'
 export { default as Layout } from '@/components/layouts/Layout'
 export { default as NavBar } from '@/components/layouts/NavBar'
+export { default as SimpleLayout } from '@/components/layouts/SimpleLayout'
