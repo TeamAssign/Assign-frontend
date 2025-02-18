1e2a8fea52a0fdb0101bb9a59fd7711a8226e598
min-s9709 <mim970929@naver.com>
2025-02-18 18:18:39 +0900

    add: 피드 info 부분 mock 데이터 추가

---
+++ src/mocks/feedUserProfileData.ts
@@ -0,0 +1,10 @@
+export const ProfileInfo = {
+  name: '김민성',
+  teams: '개발 1팀',
+  spicy: 3.5,
+  salty: 2.6,
+  sweet: 4.7,
+  pros: '저는 향신료가 너무 강하면 싫어요 저는 향신료가 너무 강하면 싫어요저는 향신료가 너무 강하면 싫어요저는 향신료가 너무 강하면 싫어요 저는 향신료가 너무 강하면 싫어요저는 향신료가 너무 강하면 싫어요',
+  cons: '저는 느끼한 음식이 싫어요... 저는 느끼한 음식이 싫어요 가나다라마',
+  profileImageUrl: 'https://placehold.co/600x400',
+}
