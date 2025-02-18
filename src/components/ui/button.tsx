8ec7f68ba164801672e869d13b94d9d87c354c63
beom <bdh3659@naver.com>
2025-02-18 16:44:08 +0900

    feat: button 컴포넌트 variant 수정

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,14 +9,9 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-main  text-white hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        sub: 'bg-sub text-white hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        default: 'bg-main text-white hover:bg-main/90',
+        sub: 'bg-sub text-white hover:bg-sub/80',
+        black: 'bg-main-black text-white hover:bg-main-black/80',
       },
       size: {
         default: 'h-10 px-4 py-2',
