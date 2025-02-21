e18347a1dbda909619926e96f5af16b4c26436ee
min-s9709 <mim970929@naver.com>
2025-02-21 16:24:28 +0900

    feat: 사람 이름 검색 시 dropdown 컴포넌트

---
+++ src/components/common/SearchDropDown/index.tsx
@@ -0,0 +1,44 @@
+import { participant } from '@/types'
+import { Ref } from 'react'
+
+interface SearchDropDownProps {
+  ref: Ref<HTMLUListElement>
+  isOpen: boolean
+  onClick: (id: number) => void
+  memberList: participant[]
+}
+
+const SearchDropDown = ({
+  ref,
+  isOpen,
+  onClick,
+  memberList,
+}: SearchDropDownProps) => {
+  if (!isOpen) return null
+
+  const handleClick = (id: number) => {
+    onClick(id)
+  }
+  return (
+    <ul
+      ref={ref}
+      className='absolute mt-1 overflow-auto flex gap-4 max-h-60 flex-col bg-white w-full z-50 border-[1px] rounded-lg border-light-gray px-4 py-2 shadow-md'
+    >
+      {memberList.map((member) => (
+        <li
+          key={member.id}
+          className='w-full px-2 py-1 hover:bg-gray-100 cursor-pointer'
+          onClick={() => handleClick(member.id)}
+        >
+          <div className='flex items-center gap-6'>
+            <img src={member.profileImage} className='w-6 h-6 rounded-full' />
+            <h2>{member.name}</h2>
+            <h2 className='text-subbody text-gray-500'>{member.team}</h2>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default SearchDropDown
