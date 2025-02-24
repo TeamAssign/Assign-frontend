a2c32b4b32fd476761af085a1e4d3bd580bf9b9f
min-s9709 <mim970929@naver.com>
2025-02-24 13:51:12 +0900

    Feat: 프로필 음식 성향 수정 form 연결

--- src/components/Feed/FeedProfileInfo/index.tsx
+++ src/components/Feed/FeedProfileInfo/index.tsx
@@ -1,7 +1,8 @@
 import DissatisfiedIcon from '@/assets/icons/DissatisfiedIcon.svg?react'
 import EditIcon from '@/assets/icons/EditIcon.svg?react'
 import SatisfiedIcon from '@/assets/icons/SatisfiedIcon.svg?react'
-import FlavorStatItem from '@/components/common/FlavorStatItem'
+import { FeedProfileEditForm, FlavorStatItem, Modal } from '@/components'
+import { useState } from 'react'
 
 interface FeedProfileInfoProps {
   profileImageUrl?: string
@@ -14,28 +15,6 @@ interface FeedProfileInfoProps {
   cons: string
 }
 
-/**
- * 팀 및 개인 피드에 들어갈 information 컴포넌트
- * @example
- * const parent = () => {
- *  return (
- *      <div className='w-full'>
-            <FeedProfileInfo
-                 profileImageUrl={ProfileInfo.profileImageUrl}
-                 teams={ProfileInfo.teams}
-                 name={ProfileInfo.name}
-                 spicy={ProfileInfo.spicy}
-                 salty={ProfileInfo.salty}
-                 sweet={ProfileInfo.sweet}
-                 pros={ProfileInfo.pros}
-                 cons={ProfileInfo.cons}
-             />
-     </div> 
-     )
- *
- * }
- */
-
 const FeedProfileInfo = ({
   profileImageUrl,
   teams,
@@ -46,6 +25,8 @@ const FeedProfileInfo = ({
   pros,
   cons,
 }: FeedProfileInfoProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   return (
     <section className='w-full flex gap-4 flex-col border-[1px] border-light-gray p-3 rounded-lg text-main-black'>
       <div className='w-full flex gap-4 items-center'>
@@ -63,16 +44,19 @@ const FeedProfileInfo = ({
             <div className='bg-sub-2 px-2 py-1 rounded-lg '>
               <span className='text-white'>{teams}</span>
             </div>
-            <EditIcon />
+            <EditIcon
+              className='cursor-pointer'
+              onClick={() => setIsModalOpen(true)}
+            />
           </div>
           {name && <span>{name}</span>}
         </div>
       </div>
       <div className='w-full flex flex-col gap-3'>
         <span>🍽️ 음식 성향</span>
-        <FlavorStatItem defaultValue={spicy} type='spicy' label='맵기' />
-        <FlavorStatItem defaultValue={salty} type='salty' label='염도' />
-        <FlavorStatItem defaultValue={sweet} type='sweet' label='달기' />
+        <FlavorStatItem defaultValue={spicy} type='spicy' label='단 맛' />
+        <FlavorStatItem defaultValue={salty} type='salty' label='짠 맛' />
+        <FlavorStatItem defaultValue={sweet} type='sweet' label='매운 맛' />
         <div className='w-full flex gap-3 text-subbody'>
           <SatisfiedIcon className='w-5 h-5 flex-shrink-0' />
           <p className='w-4/5'>{pros}</p>
@@ -82,6 +66,25 @@ const FeedProfileInfo = ({
           <p className='w-4/5'>{cons}</p>
         </div>
       </div>
+
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          title='프로필 정보 수정'
+          onClose={() => setIsModalOpen(false)}
+          content={
+            <FeedProfileEditForm
+              type={profileImageUrl ? 'person' : 'team'}
+              sweet={sweet}
+              spicy={spicy}
+              salty={salty}
+              pros={pros}
+              cons={cons}
+              profileImageUrl={profileImageUrl}
+            />
+          }
+        />
+      )}
     </section>
   )
 }
