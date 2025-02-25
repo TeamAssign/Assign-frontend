1de44861ff031b6f8aba7c86a420db02d462c67d
beom <bdh3659@naver.com>
2025-02-25 15:39:39 +0900

    chore: PIE_CHART_COLORS에 상수 추가

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -8,3 +8,14 @@ export const FOOD_CATEGORIES = [
   '패스트푸드',
   '기타',
 ]
+
+export const PIE_CHART_COLORS = [
+  '#ffb3ba',
+  '#ffdfba',
+  '#ffffba',
+  '#baffc9',
+  '#bae1ff',
+  '#ffc3a0',
+  '#e1bae1',
+  '#d9d9d9',
+]
