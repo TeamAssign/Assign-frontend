f5b25409dad65eb1fc25e4795832dbcea3706508
beom <bdh3659@naver.com>
2025-02-25 15:41:12 +0900

    feat: 홈화면 구현 중

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,14 @@
+import { PieChart } from '@/components'
+
 const Home = () => {
-  return <div>홈</div>
+  return (
+    <div className='p-4'>   
+      <h2 className="mb-1 text-xl font-bold">음식 선호도</h2>
+        <p className="mb-4 text-gray-600">직원들이 가장 선호하는 음식 종류는 무엇일까요?</p>
+        {/* 차트 컨테이너: 너비 70%, 높이 400px, 가운데 정렬 */}
+        <PieChart />
+    </div>
+  )
 }
 
 export default Home
