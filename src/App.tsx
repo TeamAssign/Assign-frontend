1c2c781e8bbd08242b162e3efc711946c1c0920e
beom <bdh3659@naver.com>
2025-02-27 14:06:27 +0900

    feat: Interceptors를 통해 헤더에 토큰 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { setTokenProvider } from '@/apis/interceptors'
 import queryClient from '@/apis/queryClient'
 import { Routes } from '@/routes'
 import '@/styles/global.css'
+import { useAuth0 } from '@auth0/auth0-react'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { useEffect } from 'react'
 
 const App = () => {
+  const { getAccessTokenSilently } = useAuth0()
+
+  useEffect(() => {
+    setTokenProvider(() => getAccessTokenSilently())
+  }, [getAccessTokenSilently])
+
   return (
     <QueryClientProvider client={queryClient}>
       <Routes />
