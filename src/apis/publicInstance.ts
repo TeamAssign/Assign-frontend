bdc2ed19babd83918c1b3bcd7b3a0cf0c6600c20
beom <bdh3659@naver.com>
2025-02-28 14:29:28 +0900

    feat: publicInstance 생성

---
+++ src/apis/publicInstance.ts
@@ -0,0 +1,8 @@
+import axios, { AxiosInstance } from 'axios'
+
+export const publicInstance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
