86b32b5c03a34034b19299071c5fadf833ae1cb0
beom <bdh3659@naver.com>
2025-02-28 20:21:09 +0900

    chore: env 출력 추가

--- src/apis/team/useGetTeam.ts
+++ src/apis/team/useGetTeam.ts
@@ -4,6 +4,8 @@ import { axiosInstance } from '../axiosInstance'
 const getTeam = async () => {
   try {
     const response = await axiosInstance.get('/teams')
+    console.log('API BASE URL:', import.meta.env.VITE_API_BASE_URL)
+
     return response.data
   } catch (error) {
     console.error('팀 리스트 조회 중 오류 발생:', error)
