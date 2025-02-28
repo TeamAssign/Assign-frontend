1b8e2cb4df3aa755c87a5db8fcfa2dc8b80a091d
beom <bdh3659@naver.com>
2025-02-28 20:26:02 +0900

    chore: dockerfile 수정

--- src/apis/user/useGetToken.ts
+++ src/apis/user/useGetToken.ts
@@ -4,6 +4,7 @@ import { publicInstance } from '../publicInstance'
 const getToken = async () => {
   try {
     const response = await publicInstance.get('/public/auth0/management-token')
+
     return response.data.data.access_token
   } catch (error) {
     console.error('토큰 클레임 확인 중 오류 발생:', error)
