8d4d61a0f2f08bc722ad872e96d1438596229212
beom <bdh3659@naver.com>
2025-02-28 19:25:43 +0900

    feat: DTO에 타입 추가

--- src/types/DTO.ts
+++ src/types/DTO.ts
@@ -1,1 +1,15 @@
 // DTO에 해당되는 타입들 여기로 분리
+export type TeamResponse = {
+  id: number
+  name: string
+}
+
+export type PostUserProps = {
+  name: string
+  teamName: string
+  spicy: number
+  salty: number
+  sweet: number
+  pros: string
+  cons: string
+}
