786a5e1c17d006d376f425a97c572b55a175d336
min-s9709 <mim970929@naver.com>
2025-03-01 18:13:48 +0900

    style: style 수정

--- src/components/Home/RecommendationBar/index.tsx
+++ src/components/Home/RecommendationBar/index.tsx
@@ -6,7 +6,7 @@ interface RecommendationProps {
 const RecommendationBar = ({ imgUrl, menu }: RecommendationProps) => {
   return (
     <div className='flex flex-col items-center w-full gap-4 p-6 border border-gray-200 rounded-lg'>
-      <img src={imgUrl} alt={menu} className='aspect-square' />
+      <img src={imgUrl} alt={menu} className='aspect-square rounded-lg' />
       <p className='text-lg font-semibold'>오늘 점심은 {menu} 어떠세요?</p>
     </div>
   )
