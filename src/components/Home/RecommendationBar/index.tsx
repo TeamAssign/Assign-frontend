1cbb9f9235ee4aa5c8ead24533ba24f17c00c166
min-s9709 <mim970929@naver.com>
2025-03-03 22:27:52 +0900

    fix: DTO로 인한 컴포넌트 수정

--- src/components/Home/RecommendationBar/index.tsx
+++ src/components/Home/RecommendationBar/index.tsx
@@ -1,13 +1,20 @@
+import { Tag } from '@/components'
+
 interface RecommendationProps {
   imgUrl: string
   menu: string
+  accuracy: number
 }
 
-const RecommendationBar = ({ imgUrl, menu }: RecommendationProps) => {
+const RecommendationBar = ({ imgUrl, menu, accuracy }: RecommendationProps) => {
   return (
     <div className='flex flex-col items-center w-full gap-4 p-6 border border-gray-200 rounded-lg'>
-      <img src={imgUrl} alt={menu} className='aspect-square rounded-lg' />
+      <img src={imgUrl} alt={menu} className='rounded-lg aspect-square' />
       <p className='text-lg font-semibold'>오늘 점심은 {menu} 어떠세요?</p>
+      <div className='flex items-center gap-3'>
+        <span className='text-body text-sub-2'>💻 AI가 분석한 취향 적합률</span>
+        <Tag color='lightGreen'>{(accuracy * 100).toFixed(1)} %</Tag>
+      </div>
     </div>
   )
 }
