988a0e9f56b301ee19f3c2373107eb06840315fd
min-s9709 <mim970929@naver.com>
2025-03-03 22:30:38 +0900

    feat: NoResult 추가

--- src/components/Recommendation/StoreRecommendation/index.tsx
+++ src/components/Recommendation/StoreRecommendation/index.tsx
@@ -1,7 +1,8 @@
-import { StoreCard } from '@/components'
+import { NoResult, StoreCard } from '@/components'
 import { MenuStoreValues } from '@/types'
 import { Navigation, Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
 
 interface StoreRecommendationProps {
   menu: string
@@ -10,25 +11,28 @@ interface StoreRecommendationProps {
 
 const StoreRecommendation = ({ menu, data }: StoreRecommendationProps) => {
   return (
-    <div className='w-full flex flex-col gap-2'>
-      <span className='text-body text-sub-2 font-bold'>
+    <div className='flex flex-col w-full gap-2'>
+      <span className='font-bold text-body text-sub-2'>
         🍽️ {menu}에 해당하는 맛집 리스트
       </span>
       <div className='w-full'>
-        <Swiper
-          modules={[Navigation, Pagination]}
-          slidesPerView={2.2}
-          slidesPerGroup={2}
-          spaceBetween={10}
-          pagination={{ clickable: true }}
-          style={{ paddingBottom: '40px' }}
-        >
-          {data.map((store, index) => (
-            <SwiperSlide key={index}>
-              <StoreCard {...store} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        {data.length === 0 && <NoResult />}
+        {data.length !== 0 && (
+          <Swiper
+            modules={[Navigation, Pagination]}
+            slidesPerView={2.2}
+            slidesPerGroup={2}
+            spaceBetween={10}
+            pagination={{ clickable: true }}
+            style={{ paddingBottom: '40px' }}
+          >
+            {data.map((store, index) => (
+              <SwiperSlide key={index}>
+                <StoreCard {...store} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </div>
   )
