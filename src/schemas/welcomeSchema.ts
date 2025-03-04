f143e6163f9f5f617e75f03daabb3cea87ae0adf
min-s9709 <mim970929@naver.com>
2025-03-04 22:32:59 +0900

    fix: 스키마 수정

--- src/schemas/welcomeSchema.ts
+++ src/schemas/welcomeSchema.ts
@@ -4,17 +4,26 @@ const WelcomeSchema = z.object({
   name: z.string().min(1, { message: '이름을 입력해주세요' }),
   teamName: z.string().min(1, { message: '팀을 선택해주세요' }),
 
-  sweet: z.number().min(0).max(5),
-  salty: z.number().min(0).max(5),
-  spicy: z.number().min(0).max(5),
+  sweet: z
+    .number()
+    .min(1, { message: '맛에 대한 성향은 최소 1 이상 설정해주세요' })
+    .max(5),
+  salty: z
+    .number()
+    .min(1, { message: '맛에 대한 성향은 최소 1 이상 설정해주세요' })
+    .max(5),
+  spicy: z
+    .number()
+    .min(1, { message: '맛에 대한 성향은 최소 1 이상 설정해주세요' })
+    .max(5),
 
   pros: z
     .string()
-    .min(15, { message: '15자 이상 입력해주세요' })
+    .min(10, { message: '10자 이상 입력해주세요' })
     .max(100, { message: '100자 이내로 입력해주세요' }),
   cons: z
     .string()
-    .min(15, { message: '15자 이상 입력해주세요' })
+    .min(10, { message: '10자 이상 입력해주세요' })
     .max(100, { message: '100자 이내로 입력해주세요' }),
 })
 
