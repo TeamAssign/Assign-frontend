69c17f732c39ed8038d6261801a2837d89f367c4
min-s9709 <mim970929@naver.com>
2025-03-06 01:53:13 +0900

    feat: 팀 및 개인 피드 리뷰 리스트 및 통계 api 연결

--- src/pages/My/index.tsx
+++ src/pages/My/index.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  CardSkeleton,
   FeedProfileInfo,
   FeedReviewBar,
   Loader,
@@ -7,45 +8,56 @@ import {
   PieChart,
   ReviewForm,
 } from '@/components'
+import useGetUserReviews from '@/hooks/apis/feed/useGetUserReviews'
+import useGetUserSummary from '@/hooks/apis/summary/useGetUserSummary'
 import useGetUserFeedInfo from '@/hooks/apis/user/useGetUserFeedInfo'
-import { personStatsData } from '@/mocks/personStatsData'
-import { reviewData } from '@/mocks/reviewData'
+import { ReviewType } from '@/types/DTO'
 import { PlusIcon } from 'lucide-react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const My = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
 
-  const { data: userFeedInfo, status } = useGetUserFeedInfo()
+  const { data: userFeedInfo, status: userInfoStatus } = useGetUserFeedInfo()
+  const {
+    data: reviewsData,
+    hasNextPage,
+    isFetchingNextPage,
+    ref,
+  } = useGetUserReviews()
 
-  if (status === 'pending') {
+  const { data: userSummary, status: userSummaryStatus } = useGetUserSummary()
+
+  console.log(userSummary)
+  console.log(reviewsData)
+  if (userInfoStatus === 'pending' || userSummaryStatus === 'pending') {
     return (
-      <div className='flex items-center justify-center w-screen h-screen'>
+      <div className='flex flex-col mx-auto min-h-screen max-w-[600px]'>
         <Loader />
       </div>
     )
   }
 
-  if (status === 'error') {
-    return <div>Error</div>
-  }
-
   return (
     <section className='flex flex-col w-full gap-4'>
-      <FeedProfileInfo
-        type='user'
-        profileImageUrl={userFeedInfo.imgurl}
-        teams={userFeedInfo.teamName}
-        name={userFeedInfo.name}
-        spicy={userFeedInfo.spicy}
-        salty={userFeedInfo.salty}
-        sweet={userFeedInfo.sweet}
-        pros={userFeedInfo.pros}
-        cons={userFeedInfo.cons}
-      />
+      {userInfoStatus === 'error' && <div>error</div>}
+      {userFeedInfo && (
+        <FeedProfileInfo
+          type='user'
+          profileImageUrl={userFeedInfo.profileImageUrl}
+          teams={userFeedInfo.teamName}
+          name={userFeedInfo.name}
+          spicy={userFeedInfo.spicy}
+          salty={userFeedInfo.salty}
+          sweet={userFeedInfo.sweet}
+          pros={userFeedInfo.pros}
+          cons={userFeedInfo.cons}
+        />
+      )}
+
       <div className='flex flex-col gap-2'>
         <span className='font-bold text-sub-2 text-title'>📊 내 통계</span>
-        <PieChart data={personStatsData} />
+        {userSummary && <PieChart data={userSummary.statistics.categories} />}
       </div>
       <div className='w-full'>
         <div className='flex items-center gap-4'>
@@ -61,11 +73,26 @@ const My = () => {
           </Button>
         </div>
 
-        <div className='flex flex-col gap-3'>
-          {reviewData.map((review, index) => (
-            <FeedReviewBar feedType='user' key={index} {...review} />
+        <div className='flex flex-col w-full gap-3'>
+          {reviewsData?.pages.map((page, pageIndex) => (
+            <React.Fragment key={pageIndex}>
+              {page.content.map((review: ReviewType) => (
+                <FeedReviewBar
+                  feedType='user'
+                  key={review.reviewId}
+                  {...review}
+                />
+              ))}
+            </React.Fragment>
           ))}
         </div>
+        {hasNextPage && (
+          <div ref={ref}>
+            <section className='w-full h-16'>
+              {isFetchingNextPage ? <CardSkeleton /> : null}
+            </section>
+          </div>
+        )}
       </div>
       {isModalOpen && (
         <Modal
