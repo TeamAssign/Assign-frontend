7677715e8c156f68ce8f3c833a8ca13a5a56cafb
min-s9709 <mim970929@naver.com>
2025-03-06 01:51:55 +0900

    feat: 프로필 수정 결과에 따른 toast 추가

--- src/hooks/apis/user/usePutUserFeedInfo.ts
+++ src/hooks/apis/user/usePutUserFeedInfo.ts
@@ -2,16 +2,19 @@ import queryClient from '@/apis/queryClient'
 import { putUserFeedInfo } from '@/apis/user/putUserFeedInfo'
 import { ProfileFormValues } from '@/schemas/profileSchema'
 import { useMutation } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
 
 const usePutUserFeedInfo = () => {
   const { mutate, status } = useMutation({
     mutationFn: (data: ProfileFormValues) => putUserFeedInfo(data),
     onSuccess: () => {
+      toast.success('프로필 수정 완료')
       queryClient.invalidateQueries({
         queryKey: ['userFeed'],
       })
     },
     onError: (error) => {
+      toast.error('프로필 수정 실패')
       console.error(error)
     },
   })
