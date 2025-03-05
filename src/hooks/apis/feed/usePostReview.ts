399bf05358ea8106e748a10a2a6180b9ba4b333d
min-s9709 <mim970929@naver.com>
2025-03-06 01:54:15 +0900

    feat: 리뷰 등록 결과 여부에 따른 toast 추가

--- src/hooks/apis/feed/usePostReview.ts
+++ src/hooks/apis/feed/usePostReview.ts
@@ -1,14 +1,16 @@
 import { postReview } from '@/apis/feed/postReview'
 import { ReviewFormValues } from '@/schemas/reviewSchema'
 import { useMutation } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
 
 const usePostReview = () => {
   const { mutate, status } = useMutation({
     mutationFn: (data: ReviewFormValues) => postReview(data),
     onSuccess: () => {
-      console.log('성공')
+      toast.success('리뷰 등록 완료')
     },
     onError: (error) => {
+      toast.error('리뷰 등록 실패')
       console.error(error)
     },
   })
