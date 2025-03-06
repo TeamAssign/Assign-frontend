4966d5c9e25867bf3e17346a5835d20ba667f1cd
min-s9709 <mim970929@naver.com>
2025-03-06 14:20:17 +0900

    chore: 불필요한 주석문 제거

--- src/components/layouts/Layout/index.tsx
+++ src/components/layouts/Layout/index.tsx
@@ -1,12 +1,6 @@
 import { Header, NavBar } from '@/components'
 import { Outlet } from 'react-router-dom'
 
-/**
- * Layout 컴포넌트
- * @description
- * 레이아웃을 잡기 위한 컴포넌트입니다 .
- */
-
 const Layout = () => {
   return (
     <div className='flex flex-col mx-auto min-h-screen max-w-[600px]'>
