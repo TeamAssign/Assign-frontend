687108bb0efb7d85dc77fe149b0ab17685dc4b7f
min-s9709 <mim970929@naver.com>
2025-03-06 14:22:21 +0900

    feat: 결과에 따른 toast 처리

--- src/apis/user/getUsersList.ts
+++ src/apis/user/getUsersList.ts
@@ -1,36 +1,14 @@
+import toast from 'react-hot-toast'
 import { axiosInstance } from '../axiosInstance'
-
-// interface User {
-//   id: number
-//   name: string
-//   teamName: string
-//   profileImageUrl: string
-// }
-
-// interface PageInfo {
-//   currentPage: number
-//   hasNextPage: boolean
-//   hasPrevPage: boolean
-//   size: number
-//   totalElements: number
-//   totalPages: number
-// }
-
-// interface ApiResponse {
-//   message: string
-//   content: User[]
-//   pageInfo: PageInfo
-//   timestam: string
-// }
 
 export const getUsersList = async ({ pageParam = 1 }) => {
   try {
     const response = await axiosInstance.get(
-      `/users/search?page=${pageParam}&size=7`,
+      `/users/search?page=${pageParam}&size=50`,
     )
     return response.data.data
   } catch (e) {
     console.error(e)
-    throw e
+    toast.error('유저 리스트를 불러오는데 실패했습니다')
   }
 }
