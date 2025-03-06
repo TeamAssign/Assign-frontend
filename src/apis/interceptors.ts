e5a0a83b71212dc4547fcb826c6e0bd496a90ab4
min-s9709 <mim970929@naver.com>
2025-03-06 14:24:09 +0900

    fix: 인증 및 429 에러 response interceptor에서 처리

--- src/apis/interceptors.ts
+++ src/apis/interceptors.ts
@@ -1,5 +1,6 @@
 import { axiosInstance } from '@/apis/axiosInstance'
 import { AxiosError, AxiosResponse } from 'axios'
+import toast from 'react-hot-toast'
 
 type GetTokenFunction = () => Promise<string>
 let tokenProvider: GetTokenFunction | null = null
@@ -25,7 +26,13 @@ axiosInstance.interceptors.request.use(async (config) => {
 axiosInstance.interceptors.response.use(
   (response: AxiosResponse) => response,
   (error: AxiosError) => {
-    console.error(error)
+    if (error.status === 429) {
+      toast.error('너무 많은 요청이 들어왔습니다. 잠시 후 다시 시도해주세요.')
+    }
+    if (error.status === 401) {
+      toast.error('로그인이 만료 되었습니다. 다시 로그인해주세요')
+      window.location.href = '/signin'
+    }
     return Promise.reject(error)
   },
 )
