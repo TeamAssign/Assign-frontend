93623f9da70f297517e34736bcdd2bee66ffabf4
min-s9709 <mim970929@naver.com>
2025-03-07 02:44:33 +0900

    fix: 이미지 에러시 기본 이미지 적용

--- src/components/Recommendation/MenuRecommendation/index.tsx
+++ src/components/Recommendation/MenuRecommendation/index.tsx
@@ -1,4 +1,6 @@
 import { Tag } from '@/components'
+import { LOGO_IMAGE_URL } from '@/constant'
+import { useState } from 'react'
 
 interface MenuRecommendationProps {
   name: string
@@ -11,6 +13,12 @@ const MenuRecommendation = ({
   imageUrl,
   accuracy,
 }: MenuRecommendationProps) => {
+  const [imageSrc, setImageSrc] = useState(imageUrl || LOGO_IMAGE_URL)
+
+  const handleImageError = () => {
+    setImageSrc(LOGO_IMAGE_URL)
+  }
+
   return (
     <div className='w-full flex flex-col items-center gap-3 border-b-[1px] border-light-gray py-3 rounded-lg'>
       <span className='font-bold text-body text-sub-2'>
@@ -20,8 +28,9 @@ const MenuRecommendation = ({
       <div className='flex flex-col items-center w-full gap-3'>
         <img
           className='w-[200px] h-[200px] rounded-lg'
-          src={imageUrl}
+          src={imageSrc}
           alt='음식 사진'
+          onError={handleImageError}
         />
         <div className='flex items-center gap-3'>
           <span className='text-body text-sub-2'>
